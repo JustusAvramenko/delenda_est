788db78efc6f104bd7df4ef5c58642da8a9ba809
Justus Avramenko <michaeldhafer@gmail.com>
2017-11-19 20:09:36 -0500

    There was once a dream that was Rome. You could only whisper it.

--- maps/scripts/ConquestCivicCenters.js
+++ maps/scripts/ConquestCivicCenters.js
@@ -1,9 +1,5 @@
-var cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
-cmpTrigger.conquestClassFilter = "CivilCentre";
-cmpTrigger.conquestDefeatReason = markForTranslation("%(player)s has been defeated (lost all Civic Centers).");
-
-var data = {"enabled": true};
-cmpTrigger.RegisterTrigger("OnOwnershipChanged", "ConquestHandlerOwnerShipChanged", data);
-cmpTrigger.RegisterTrigger("OnStructureBuilt", "ConquestAddStructure", data);
-
-cmpTrigger.DoAfterDelay(0, "ConquestStartGameCount", null);
+{
+	let cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
+	cmpTrigger.conquestClassFilter = "CivilCentre";
+	cmpTrigger.conquestDefeatReason = markForTranslation("%(player)s has been defeated (lost all Civic Centers).");
+}
