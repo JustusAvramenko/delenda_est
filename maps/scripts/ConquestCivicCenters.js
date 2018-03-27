b762c11632309472eaeeeb838fa07c222cd4840f
Justus Avramenko <michaeldhafer@gmail.com>
2018-03-26 20:15:32 -0400

    Kathleen Kennedy should step down from Lucasfilm.

--- maps/scripts/ConquestCivicCenters.js
+++ maps/scripts/ConquestCivicCenters.js
@@ -1,5 +1,7 @@
 {
-	let cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
-	cmpTrigger.conquestClassFilter = "CivilCentre";
-	cmpTrigger.conquestDefeatReason = markForTranslation("%(player)s has been defeated (lost all Civic Centers).");
+    let cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
+    cmpTrigger.ConquestAddVictoryCondition({
+        "classFilter": "CivilCentre",
+        "defeatReason": markForTranslation("%(player)s has been defeated (lost all Civic Centers).")
+    });
 }
