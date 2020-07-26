36116096f9452a29147388dbba421e6bcad605e9
Justus Avramenko <michaeldhafer@gmail.com>
2020-07-26 04:00:43 -0400

    bunch o stuff

--- simulation/components/ResourceTrickle.js
+++ simulation/components/ResourceTrickle.js
@@ -11,15 +11,13 @@ ResourceTrickle.prototype.Schema =
 
 ResourceTrickle.prototype.Init = function()
 {
-	this.ComputeRates();
-
-	let cmpTimer = Engine.QueryInterface(SYSTEM_ENTITY, IID_Timer);
- 	cmpTimer.SetInterval(this.entity, IID_ResourceTrickle, "Trickle", this.GetTimer(), this.GetTimer(), undefined);
+	this.trickleInterval = +this.template.Interval;
+	this.CheckTimer();
 };
 
-ResourceTrickle.prototype.GetTimer = function()
+ResourceTrickle.prototype.GetInterval = function()
 {
-	return +this.template.Interval;
+	return this.trickleInterval;
 };
 
 ResourceTrickle.prototype.GetRates = function()
@@ -27,11 +25,24 @@ ResourceTrickle.prototype.GetRates = function()
 	return this.rates;
 };
 
+/**
+ * @return {boolean} - Whether this entity has at least one non-zero trickle rate.
+ */
 ResourceTrickle.prototype.ComputeRates = function()
 {
 	this.rates = {};
+	let hasTrickle = false;
 	for (let resource in this.template.Rates)
-		this.rates[resource] = ApplyValueModificationsToEntity("ResourceTrickle/Rates/"+resource, +this.template.Rates[resource], this.entity);
+	{
+		let rate = ApplyValueModificationsToEntity("ResourceTrickle/Rates/" + resource, +this.template.Rates[resource], this.entity);
+		if (rate)
+		{
+			this.rates[resource] = rate;
+			hasTrickle = true;
+		}
+	}
+
+	return hasTrickle;
 };
 
 ResourceTrickle.prototype.Trickle = function(data, lateness)
@@ -49,7 +60,48 @@ ResourceTrickle.prototype.OnValueModification = function(msg)
 	if (msg.component != "ResourceTrickle")
 		return;
 
-	this.ComputeRates();
+	this.CheckTimer();
+};
+
+ResourceTrickle.prototype.CheckTimer = function()
+{
+	if (!this.ComputeRates())
+	{
+		if (!this.timer)
+			return;
+
+		let cmpTimer = Engine.QueryInterface(SYSTEM_ENTITY, IID_Timer);
+		cmpTimer.CancelTimer(this.timer);
+		delete this.timer;
+		return;
+	}
+
+	let oldTrickleInterval = this.trickleInterval;
+	this.trickleInterval = ApplyValueModificationsToEntity("ResourceTrickle/Interval", +this.template.Interval, this.entity);
+	if (this.trickleInterval < 0)
+	{
+		let cmpTimer = Engine.QueryInterface(SYSTEM_ENTITY, IID_Timer);
+		cmpTimer.CancelTimer(this.timer);
+		delete this.timer;
+		return;
+	}
+
+	if (this.timer)
+	{
+		if (this.trickleInterval == oldTrickleInterval)
+			return;
+
+		// If the timer wasn't invalidated before (interval <= 0), just update it.
+		if (oldTrickleInterval > 0)
+		{
+			let cmpTimer = Engine.QueryInterface(SYSTEM_ENTITY, IID_Timer);
+			cmpTimer.UpdateRepeatTime(this.timer, this.trickleInterval);
+			return;
+		}
+	}
+
+	let cmpTimer = Engine.QueryInterface(SYSTEM_ENTITY, IID_Timer);
+	this.timer = cmpTimer.SetInterval(this.entity, IID_ResourceTrickle, "Trickle", this.trickleInterval, this.trickleInterval, undefined);
 };
 
 Engine.RegisterComponentType(IID_ResourceTrickle, "ResourceTrickle", ResourceTrickle);
