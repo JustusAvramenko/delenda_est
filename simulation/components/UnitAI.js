597a09042ff60763ee120ed6730e14c06ae8f34c
Justus Avramenko <michaeldhafer@gmail.com>
2020-08-19 03:52:06 -0400

    tweaks

--- simulation/components/UnitAI.js
+++ simulation/components/UnitAI.js
@@ -1892,7 +1892,7 @@ UnitAI.prototype.UnitFsmSpec = {
 							this.FinishOrder();
 							return;
 						}
-						else if (!this.order.data.force)
+						else if (!this.order.data.force || !this.order.data.lastPos)
 						{
 							this.SetNextState("COMBAT.FINDINGNEWTARGET");
 							return;
