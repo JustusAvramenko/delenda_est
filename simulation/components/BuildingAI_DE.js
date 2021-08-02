e9eacec0526d780e40f1425d8565fd69a43bb2fb
Justus Avramenko <michaeldhafer@gmail.com>
2021-08-02 02:29:17 -0400

    Make buildings stop shooting at nearby animals

---
+++ simulation/components/BuildingAI_DE.js
@@ -0,0 +1,34 @@
+BuildingAI.prototype.OnRangeUpdate = function(msg)
+{
+
+	var cmpAttack = Engine.QueryInterface(this.entity, IID_Attack);
+	if (!cmpAttack)
+		return;
+
+	// Target enemy units except animals.
+	if (msg.tag == this.gaiaUnitsQuery)
+	{
+		msg.added = msg.added.filter(e => {
+			let cmpUnitAI = Engine.QueryInterface(e, IID_UnitAI);
+			return cmpUnitAI && (!cmpUnitAI.IsAnimal()); // << This has changed.
+		});
+	}
+	else if (msg.tag != this.enemyUnitsQuery)
+		return;
+
+	// Add new targets.
+	for (let entity of msg.added)
+		if (cmpAttack.CanAttack(entity))
+			this.targetUnits.push(entity);
+
+	// Remove targets outside of vision-range.
+	for (let entity of msg.removed)
+	{
+		let index = this.targetUnits.indexOf(entity);
+		if (index > -1)
+			this.targetUnits.splice(index, 1);
+	}
+
+	if (this.targetUnits.length)
+		this.StartTimer();
+};
