10a79f736f618ae626e4f13d55885f6fa8dcf6c1
Justus Avramenko <michaeldhafer@gmail.com>
2021-09-11 01:49:11 -0400

    Remove old Allied Cartography patch. It is now superfluous.

--- gui/gamesetup/Pages/GameSetupPage/Panels/GameDescription_CartographyMode.js
+++
@@ -1,8 +0,0 @@
-{
-	let registerWatchersOld = GameDescription.prototype.registerWatchers;
-	GameDescription.prototype.registerWatchers = function () {
-		registerWatchersOld.call(this);
-		let update = () => this.updateGameDescription();
-		g_GameSettings.mapExploration.watch(update, ["allied"]);
-	}
-}
