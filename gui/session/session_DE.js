dcd438f5b0d50cff6a55762d1714612e7bcebd1a
Justus Avramenko <michaeldhafer@gmail.com>
2021-10-24 04:21:14 -0400

    Update session_DE.js to put Minister icons back at the top of the UI

--- gui/session/session_DE.js
+++ gui/session/session_DE.js
@@ -2,3 +2,8 @@
  * Unit classes to be checked for the idle-worker-hotkey.
  */
 var g_WorkerTypes = ["IdleWorker"];
+
+/**
+ * Order in which the panel entities are shown.
+ */
+var g_PanelEntityOrder = ["Hero", "Minister", "Relic"];
