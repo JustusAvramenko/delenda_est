5644ad81fc56605bc80d9cda6f651af3a80855d9
Justus Avramenko <michaeldhafer@gmail.com>
2022-02-20 17:06:17 -0500

    Fix PlayerCiv.js after 26438

--- gui/gamesetup/Pages/GameSetupPage/GameSettings/PerPlayer/Dropdowns/PlayerCiv.js
+++ gui/gamesetup/Pages/GameSetupPage/GameSettings/PerPlayer/Dropdowns/PlayerCiv.js
@@ -1,48 +1,66 @@
 PlayerSettingControls.PlayerCiv = class PlayerCiv extends GameSettingControlDropdown
 {
-	constructor(...args)
-	{
-		super(...args);
+    constructor(...args)
+    {
+        super(...args);
 
-		this.values = prepareForDropdown(this.getItems());
+        g_GameSettings.playerCiv.watch(() => this.rebuild(), ["values", "locked"]);
 
-		this.dropdown.list = this.values.name;
-		this.dropdown.list_data = this.values.civ;
+        this.items = this.getItems(false);
+        this.allItems = this.getItems(true);
+        this.wasLocked = undefined;
 
-		g_GameSettings.playerCiv.watch(() => this.render(), ["values", "locked"]);
-		this.render();
-	}
+        this.rebuild();
+    }
 
-	setControl()
-	{
-		this.label = Engine.GetGUIObjectByName("playerCivText[" + this.playerIndex + "]");
-		this.dropdown = Engine.GetGUIObjectByName("playerCiv[" + this.playerIndex + "]");
-	}
+    setControl()
+    {
+        this.label = Engine.GetGUIObjectByName("playerCivText[" + this.playerIndex + "]");
+        this.dropdown = Engine.GetGUIObjectByName("playerCiv[" + this.playerIndex + "]");
+    }
 
-	onHoverChange()
-	{
-		this.dropdown.tooltip = this.values && this.values.tooltip[this.dropdown.hovered] || this.Tooltip;
-	}
+    onHoverChange()
+    {
+        this.dropdown.tooltip = this.values && this.values.tooltip[this.dropdown.hovered] || this.Tooltip;
+    }
 
-	render()
-	{
-		this.setEnabled(!g_GameSettings.playerCiv.locked[this.playerIndex]);
-		this.setSelectedValue(g_GameSettings.playerCiv.values[this.playerIndex]);
-	}
+    rebuild()
+    {
+        const isLocked = g_GameSettings.playerCiv.locked[this.playerIndex];
+        if (this.wasLocked !== isLocked)
+        {
+            this.wasLocked = isLocked;
+            this.values = prepareForDropdown(isLocked ? this.allItems : this.items);
 
-	getItems()
-	{
-		let values = [];
+            this.dropdown.list = this.values.name;
+            this.dropdown.list_data = this.values.civ;
 
-		for (let civ in g_CivData)
-			if (g_CivData[civ].SelectableInGameSetup)
-				values.push({
-					"name": g_CivData[civ].Name,
-					"autocomplete": g_CivData[civ].Name,
-					"tooltip": g_CivData[civ].History,
-					"civ": civ,
+            // If not locked, reset selection, else we can end up with empty label.
+            if (!isLocked && g_IsController && g_GameSettings.playerCiv.values[this.playerIndex])
+                this.onSelectionChange(0);
+        }
+        this.render();
+    }
+
+    render()
+    {
+        this.setEnabled(!g_GameSettings.playerCiv.locked[this.playerIndex]);
+        this.setSelectedValue(g_GameSettings.playerCiv.values[this.playerIndex]);
+    }
+
+    getItems(allItems)
+    {
+        let values = [];
+
+        for (let civ in g_CivData)
+            if (allItems || g_CivData[civ].SelectableInGameSetup)
+                values.push({
+                    "name": g_CivData[civ].Name,
+                    "autocomplete": g_CivData[civ].Name,
+                    "tooltip": g_CivData[civ].History,
+                    "civ": civ,
 					"random": false
-				});
+                });
 
 		values.sort(sortNameIgnoreCase);
 
@@ -58,39 +76,39 @@ PlayerSettingControls.PlayerCiv = class PlayerCiv extends GameSettingControlDrop
 
 		values.unshift(... random_civ_groups);
 
-		values.unshift({
-			"name": setStringTags(this.RandomCivCaption, this.RandomItemTags),
-			"autocomplete": this.RandomCivCaption,
-			"tooltip": this.RandomCivTooltip,
-			"civ": this.RandomCivId,
+        values.unshift({
+            "name": setStringTags(this.RandomCivCaption, this.RandomItemTags),
+            "autocomplete": this.RandomCivCaption,
+            "tooltip": this.RandomCivTooltip,
+            "civ": this.RandomCivId,
 			'random': true
-		});
+        });
 
-		return values;
-	}
+        return values;
+    }
 
-	getAutocompleteEntries()
-	{
-		return this.values.autocomplete;
-	}
+    getAutocompleteEntries()
+    {
+        return this.values.autocomplete;
+    }
 
-	onSelectionChange(itemIdx)
-	{
-		g_GameSettings.playerCiv.setValue(this.playerIndex, this.values.civ[itemIdx]);
-		this.gameSettingsController.setNetworkInitAttributes();
-	}
+    onSelectionChange(itemIdx)
+    {
+        g_GameSettings.playerCiv.setValue(this.playerIndex, this.values.civ[itemIdx]);
+        this.gameSettingsController.setNetworkInitAttributes();
+    }
 };
 
 PlayerSettingControls.PlayerCiv.prototype.Tooltip =
-	translate("Choose the civilization for this player.");
+    translate("Choose the civilization for this player.");
 
 PlayerSettingControls.PlayerCiv.prototype.RandomCivCaption =
-	translateWithContext("civilization", "Random");
+    translateWithContext("civilization", "Random");
 
 PlayerSettingControls.PlayerCiv.prototype.RandomCivId =
-	"random";
+    "random";
 
 PlayerSettingControls.PlayerCiv.prototype.RandomCivTooltip =
-	translate("Picks one civilization at random when the game starts.");
+    translate("Picks one civilization at random when the game starts.");
 
 PlayerSettingControls.PlayerCiv.prototype.AutocompleteOrder = 90;
