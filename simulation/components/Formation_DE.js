3cd742b98caa77a84ba16d324beb25d9ed85fe0a
Justus Avramenko <michaeldhafer@gmail.com>
2022-03-25 17:47:32 -0400

    Delete a bunch of now-duplicate files!

--- simulation/components/Formation_DE.js
+++
@@ -1,9 +0,0 @@
-Formation.prototype.OnGlobalOwnershipChanged = function(msg)
-{
-	// When an entity is captured or destroyed, it should no longer be
-	// controlled by this formation.
-	if (this.members.indexOf(msg.entity) != -1)
-		this.RemoveMembers([msg.entity]);
-	if (msg.entity === this.entity && msg.to > -1)
-		Engine.QueryInterface(this.entity, IID_Visual)?.SetVariant("animationVariant", QueryPlayerIDInterface(msg.to, IID_Identity).GetCiv());
-};
