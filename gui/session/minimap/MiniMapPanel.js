022f3b46e9daa0a269d5582e95eb96892a93bdfe
Justus Avramenko <michaeldhafer@gmail.com>
2022-10-24 21:21:57 -0400

    Add civ-specific backgrounds to the minimap

---
+++ gui/session/minimap/MiniMapPanel.js
@@ -0,0 +1,37 @@
+/**
+ * This class is concerned with managing the different elements of the minimap panel.
+ */
+class MiniMapPanel
+{
+	constructor(playerViewControl, diplomacyColors, idleWorkerClasses)
+	{
+		this.diplomacyColorsButton = new MiniMapDiplomacyColorsButton(diplomacyColors);
+		this.idleWorkerButton = new MiniMapIdleWorkerButton(playerViewControl, idleWorkerClasses);
+		this.flareButton = new MiniMapFlareButton(playerViewControl);
+		this.miniMap = new MiniMap();
+		playerViewControl.registerViewedPlayerChangeHandler(this.rebuild.bind(this));
+		registerHotkeyChangeHandler(this.rebuild.bind(this));
+	}
+
+	flare(target, playerID)
+	{
+		return this.miniMap.flare(target, playerID);
+	}
+
+	isMouseOverMiniMap()
+	{
+		return this.miniMap.isMouseOverMiniMap();
+	}
+
+	rebuild()
+	{
+		this.setCivBackgroundTexture();
+	}
+
+	setCivBackgroundTexture()
+	{
+		const playerCiv = g_ViewedPlayer > 0 ? g_Players[g_ViewedPlayer].civ : "gaia";
+		const backgroundObject = Engine.GetGUIObjectByName("minimapBackgroundTexture");
+		backgroundObject.sprite = `stretched:session/civ_minimap/background_circle_${playerCiv}.png`;
+	}
+}
