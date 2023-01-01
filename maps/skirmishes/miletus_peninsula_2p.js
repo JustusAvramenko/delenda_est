9521ebc9dc61b919430f1723defdd4518402d810
Justus Avramenko <michaeldhafer@gmail.com>
2023-01-01 01:34:39 -0500

    Add a Trigger Script to Miletus Peninsula that spawns Pirate Ships

---
+++ maps/skirmishes/miletus_peninsula_2p.js
@@ -0,0 +1,43 @@
+Trigger.prototype.SpawnAndAttack = function()
+{
+	var intruders = TriggerHelper.SpawnUnitsFromTriggerPoints(
+			pickRandom(["B", "C"]), "units/pirates/ship_bireme", this.attackSize, 0);
+
+	for (var origin in intruders)
+	{
+		var playerID = TriggerHelper.GetOwner(+origin);
+		var cmd = null;
+		for (var target of this.GetTriggerPoints("A"))
+		{
+			if (TriggerHelper.GetOwner(target) != playerID)
+				continue;
+			var cmpPosition = Engine.QueryInterface(target, IID_Position);
+			if (!cmpPosition || !cmpPosition.IsInWorld)
+				continue;
+			// store the x and z coordinates in the command
+			cmd = cmpPosition.GetPosition();
+			break;
+		}
+		if (!cmd)
+			continue;
+		cmd.type = "attack-walk";
+		cmd.entities = intruders[origin];
+		cmd.targetClasses = { "attack": ["Unit", "Structure"] };
+		cmd.allowCapture = false;
+		cmd.queued = true;
+		ProcessCommand(0, cmd);
+	}
+
+	// enlarge the attack time and size
+	var rand = randFloat(1, 3);
+	this.attackTime *= rand;
+	this.attackSize = Math.round(this.attackSize * rand);
+	this.DoAfterDelay(this.attackTime, "SpawnAndAttack", {});
+};
+
+{
+	let cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
+	cmpTrigger.attackSize = 1; // attack with 1 soldier
+	cmpTrigger.attackTime = 600 * 1000; // attack in 10 minutes
+	cmpTrigger.DoAfterDelay(cmpTrigger.attackTime, "SpawnAndAttack", {});
+}
