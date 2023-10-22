4d90fad3f690e5e70a3f14c0cf911486c20882a6
Justus Avramenko <JustusAvramenko@users.noreply.github.com>
2023-10-22 18:13:11 -0400

    Add smoke to Roman and Kushite backgrounds

--- gui/pregame/backgrounds/badass_romans.js
+++ gui/pregame/backgrounds/badass_romans.js
@@ -6,6 +6,11 @@ g_BackgroundLayerData.push(
 			"tiling": false,
 		},
 		{
+			"offset": (time, width) => 0.20 * width * Math.cos(0.08 * time) + width/16,
+			"sprite": "background-romans-smoke",
+			"tiling": false,
+		},
+		{
 			"offset": (time, width) => 0.16 * width * Math.cos(0.05 * time) + width/16,
 			"sprite": "background-romans_carthage1-3",
 			"tiling": false,
