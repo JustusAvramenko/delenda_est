9cb4e21dc5f3e02c226b1d4eb9859d814fdd8cfe
wowgetoffyourcellphone <wowgetoffyourcellphone@gmail.com>
2025-06-16 19:35:15 -0400

    Cycladic Archipelago trigger script works now.
    @trompetin17

--- maps/skirmishes/cycladic_archipelago_2p.js
+++ maps/skirmishes/cycladic_archipelago_2p.js
@@ -1,5 +1,11 @@
 Trigger.prototype.SpawnUnits = function()
 {
-	var intruders = TriggerHelper.SpawnUnitsFromTriggerPoints(
-			pickRandom(["A", "B", "C", "D"]), "gaia/treasure/special_argo", this.Count, 1);
+	var argoTreasures = TriggerHelper.SpawnUnitsFromTriggerPoints(
+		pickRandom(["A", "B", "C", "D"]), "gaia/treasure/special_argo", this.unitsForSpawn, 0);
+};
+
+{
+	const cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
+	cmpTrigger.unitsForSpawn = 1; // spawn 1 unit
+	cmpTrigger.RegisterTrigger("OnInitGame", "SpawnUnits", { "enabled": true }); // at match initialization
 }
