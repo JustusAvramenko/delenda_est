8ef4f09f10b84b4cf69b03bb6cd3aa22492b7c47
justjuangui <servicios@juacarvajal.com>
2025-07-03 22:27:59 -0500

    Fix backgounds with new module supports
    After commit in 0ad 0fcc4f5fc5eb1fca0205bace03ac39a7d134c287
    backgrounds needs to override with the new "append" way.
    so basically you take the basefile.js and add
    basefile~something.append.js
    
    so this commit do two things, first clear backgrounds(~0.append.js) and
    then populate your backgrounds.

---
+++ gui/pregame/backgrounds/background~badass_actium.append.js
@@ -0,0 +1,7 @@
+backgrounds["badass_actium"] = [
+	{
+		"offset": (time, width) => 0.0 * width * Math.cos(0.0 * time),
+		"sprite": "background-actium1-1",
+		"tiling": false,
+	},
+];
--- gui/pregame/backgrounds/badass_actium.js
+++
@@ -1,8 +0,0 @@
-g_BackgroundLayerData.push(
-	[
-		{
-			"offset": (time, width) => 0.0 * width * Math.cos(0.0 * time),
-			"sprite": "background-actium1-1",
-			"tiling": false,
-		},
-	]);
