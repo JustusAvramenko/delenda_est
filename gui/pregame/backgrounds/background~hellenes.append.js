f97f172bd51fbf6e844b9ae52dfed295a49894d2
wowgetoffyourcellphone <wowgetoffyourcellphone@gmail.com>
2025-09-06 20:28:32 -0400

    New updated Hellenes main menu background

---
+++ gui/pregame/backgrounds/background~hellenes.append.js
@@ -0,0 +1,27 @@
+backgrounds['hellenes'] = [
+	{
+		"offset": (time, width) => 0.02 * width * Math.cos(0.05 * time),
+		"sprite": "background-hellenes1-1",
+		"tiling": false,
+	},
+	{
+		"offset": (time, width) => 0.20 * width * Math.cos(0.04 * time) + width/16,
+		"sprite": "smoke-orange",
+		"tiling": false,
+	},
+	{
+		"offset": (time, width) => 0.10 * width * Math.cos(0.08 * time) - width / 10,
+		"sprite": "background-hellenes1-2",
+		"tiling": false,
+	},
+	{
+		"offset": (time, width) => 0.16 * width * Math.cos(0.15 * time) + width/16,
+		"sprite": "smoke-gray",
+		"tiling": false,
+	},
+	{
+		"offset": (time, width) => 0.14 * width * Math.cos(0.05 * time) + width / 4,
+		"sprite": "background-hellenes1-3",
+		"tiling": false,
+	},
+];
---
+++ gui/pregame/backgrounds/background~kush.append.js
@@ -0,0 +1,27 @@
+backgrounds['kush'] = [
+	{
+		"offset": (time, width) => 0.07 * width * Math.cos(0.1 * time),
+		"sprite": "background-kush1-1",
+		"tiling": true
+	},
+	{
+		"offset": (time, width) => 0.05 * width * Math.cos(0.1 * time),
+		"sprite": "background-kush1-2",
+		"tiling": true
+	},
+	{
+		"offset": (time, width) => 0.16 * width * Math.cos(0.15 * time) + width/16,
+		"sprite": "smoke-orange",
+		"tiling": false,
+	},
+	{
+		"offset": (time, width) => 0.04 * width * Math.cos(0.1 * time) + 0.01 * width * Math.cos(0.04 * time),
+		"sprite": "background-kush1-3",
+		"tiling": true
+	},
+	{
+		"offset": (time, width) => -0.1,
+		"sprite": "background-kush1-4",
+		"tiling": true
+	},
+];
