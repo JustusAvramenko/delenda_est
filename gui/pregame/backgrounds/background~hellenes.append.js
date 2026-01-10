19fe3596d63d6166df006d9240d5bd9d2d5757d2
wowgetoffyourcellphone <wowgetoffyourcellphone@gmail.com>
2026-01-09 19:23:17 -0500

    A greatly improved Hellenes background

--- gui/pregame/backgrounds/background~hellenes.append.js
+++ gui/pregame/backgrounds/background~hellenes.append.js
@@ -5,23 +5,23 @@ backgrounds['hellenes'] = [
 		"tiling": false,
 	},
 	{
-		"offset": (time, width) => 0.20 * width * Math.cos(0.04 * time) + width/16,
-		"sprite": "smoke-orange",
-		"tiling": false,
-	},
-	{
 		"offset": (time, width) => 0.10 * width * Math.cos(0.08 * time) - width / 10,
 		"sprite": "background-hellenes1-2",
 		"tiling": false,
 	},
 	{
-		"offset": (time, width) => 0.16 * width * Math.cos(0.15 * time) + width/16,
-		"sprite": "smoke-gray",
+		"offset": (time, width) => 0.20 * width * Math.cos(0.04 * time) + width/16,
+		"sprite": "smoke-orange",
 		"tiling": false,
 	},
 	{
 		"offset": (time, width) => 0.14 * width * Math.cos(0.05 * time) + width / 4,
 		"sprite": "background-hellenes1-3",
+		"tiling": false,
+	},
+	{
+		"offset": (time, width) => 0.16 * width * Math.cos(0.15 * time) + width/16,
+		"sprite": "smoke-gray",
 		"tiling": false,
 	},
 ];
