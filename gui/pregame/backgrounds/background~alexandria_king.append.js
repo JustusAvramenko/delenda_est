c365b01855c822142e57a49011a6932da5e6437e
wowgetoffyourcellphone <wowgetoffyourcellphone@gmail.com>
2026-01-12 06:05:39 -0500

    New main menu background based on Ptolemy III

---
+++ gui/pregame/backgrounds/background~alexandria_king.append.js
@@ -0,0 +1,17 @@
+backgrounds['alexandria_king'] = [
+	{
+		"offset": (time, width) => 0.0 * width * Math.cos(0.0 * time) + width/16,
+		"sprite": "alexandria2-1",
+		"tiling": false,
+	},
+	{
+		"offset": (time, width) => 0.20 * width * Math.cos(0.08 * time) + width/16,
+		"sprite": "smoke-gray-king",
+		"tiling": false,
+	},
+	{
+		"offset": (time, width) => 0.16 * width * Math.cos(0.05 * time) + width/16,
+		"sprite": "alexandria2-2",
+		"tiling": false,
+	},
+];
