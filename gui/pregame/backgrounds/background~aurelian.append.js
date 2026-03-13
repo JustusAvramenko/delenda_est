24206ddab0da4bcf5518ae1eeff52477da2bb163
wowgetoffyourcellphone <wowgetoffyourcellphone@gmail.com>
2026-03-13 06:36:32 -0400

    More Augustus and Aurelian stuff

---
+++ gui/pregame/backgrounds/background~aurelian.append.js
@@ -0,0 +1,17 @@
+backgrounds['aurelian'] = [
+	{
+		"offset": (time, width) => 0.0 * width * Math.cos(0.0 * time) + width/16,
+		"sprite": "aurelian-bg",
+		"tiling": false,
+	},
+	{
+		"offset": (time, width) => 0.20 * width * Math.cos(0.12 * time) + width/16,
+		"sprite": "aurelian-smoke",
+		"tiling": false,
+	},
+	{
+		"offset": (time, width) => 0.16 * width * Math.cos(0.05 * time) + width/16,
+		"sprite": "aurelian",
+		"tiling": false,
+	},
+];
